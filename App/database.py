1fedba2b25ff4b00a4650473c0e86d8f2d8d7553
fuji1o <fuji.o@yandex.ru>
2025-05-15 22:13:42 +0500

    feat:настройка подключения SQLite

---
+++ App/database.py
@@ -0,0 +1,7 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker
+
+DATABASE_URL = "sqlite:///cmd_todo_list.db"
+engine = create_engine(DATABASE_URL, echo=False)
+Session = sessionmaker(bind=engine)
+Base = declarative_base()
